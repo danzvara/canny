9eadf4c406400bf3e816f81d9f09db9928cec86d
Daniel Zvara <dan.zvara@gmail.com>
2017-02-04 02:47:41 +0000

    Main: apply Canny to the stream
    Signed-off-by: Daniel Zvara <dan.zvara@gmail.com>

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,11 @@
 #include <iostream>
-#include <camera_stream.hpp>
+#include "camera_stream.hpp"
 
 int main (int argc, char** argv)
 {
   CameraStream cameraStream;
+  Canny canny;
+  cameraStream.useCanny(canny);
   cameraStream.run();
 
   return 0;
