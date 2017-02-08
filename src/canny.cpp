35f6354776cb958dfcbb4dd05823204002e15afe
Daniel Zvara <dan.zvara@gmail.com>
2017-02-08 20:31:16 +0000

    canny: separate gradient calculations
    Signed-off-by: Daniel Zvara <dan.zvara@gmail.com>

--- src/canny.cpp
+++ src/canny.cpp
@@ -6,26 +6,38 @@ Canny::Canny(int threshold) : threshold(threshold) {};
 
 cv::Mat& Canny::detect(cv::Mat& src)
 {
-  const int n = 5;
-  const double k = 0.00629;
-  int kernelData[n][n] = {{2, 4, 5, 4, 2},
+  const double gauss_k = 0.00629;
+  int kernelData[5][5] = {{2, 4, 5, 4, 2},
                       {4, 9, 12, 9, 4},
                       {5, 12, 15, 12, 5},
                       {4, 9, 12, 9, 4},
                       {2, 4, 5, 4, 2}};
-  cv::Mat kernel = cv::Mat(n, n, CV_32SC1, kernelData);
+  int kernel_dataGY[3][3] = {{-1, -2, -1},
+                                {0, 0, 0},
+                                {1, 2, 1}};
+  int kernel_dataGX[3][3] = {{-1, 0, 1},
+                                {-2, 0, 2},
+                                {-1, 0, 1}};
+  cv::Mat kernelGX = cv::Mat(3, 3, CV_32SC1, kernel_dataGX);
+  cv::Mat kernelGY = cv::Mat(3, 3, CV_32SC1, kernel_dataGY);
+  cv::Mat kernelGauss = cv::Mat(5, 5, CV_32SC1, kernelData);
+
   cv::cvtColor(src, src, cv::COLOR_RGB2GRAY, 0);
-  cv::Mat& frame = applyKernel(src, kernel, n, k);
-  sobel(frame);
+  cv::Mat& frame = applyKernel(src, kernelGauss, 5, gauss_k);
+  cv::Mat& gradientX = applyKernel(frame, kernelGX, 3, 1);
+  cv::Mat& gradientY = applyKernel(frame, kernelGY, 3, 1);
+  cv::Mat& gradMag = sobel(gradientX, gradientY);
+
   //applyThreshold(frame, 240);
-  return frame;
+  return gradMag;
 }
 
 /* Applies given kernel on the image. Assumes the kernel is of dimension size x size.
  *
  * src - uchar, single channel
  * kernel - 32bit signed integer, single channel */
-cv::Mat& Canny::applyKernel(cv::Mat& src, cv::Mat& kernel, const int size, const double k)
+cv::Mat&
+Canny::applyKernel (cv::Mat& src, cv::Mat& kernel, const int size, const double k)
 {
   if (kernel.rows < 1 || kernel.cols < 1 || size < 1)
     return src;
@@ -59,37 +71,27 @@ cv::Mat& Canny::applyKernel(cv::Mat& src, cv::Mat& kernel, const int size, const
 /* Applies sobel filter on the source image.
  *
  * src - uchar single channel */
-void Canny::sobel(cv::Mat& src)
+cv::Mat&
+Canny::sobel (cv::Mat& gradientX, cv::Mat& gradientY)
 {
-  const int n = 3;
-  const double k = 1;
-  int kernel_dataGY[n][n] = {{-1, -2, -1},
-                                {0, 0, 0},
-                                {1, 2, 1}};
-  int kernel_dataGX[n][n] = {{-1, 0, 1},
-                                {-2, 0, 2},
-                                {-1, 0, 1}};
-  cv::Mat kernelGX = cv::Mat(n, n, CV_32SC1, kernel_dataGX);
-  cv::Mat kernelGY = cv::Mat(n, n, CV_32SC1, kernel_dataGY);
-
-  cv::Mat& gradX = applyKernel(src, kernelGX, n, k);
-  cv::Mat& gradY = applyKernel(src, kernelGY, n, k);
-
-  for (int i = 1; i < src.rows - 1; i++)
+  int R = gradientX.rows;
+  int C = gradientX.cols;
+  cv::Mat* dest = new cv::Mat(R, C, CV_8UC1);
+  for (int i = 1; i < R - 1; i++)
   {
-    Vec1b* gx_row = gradX.ptr<Vec1b>(i);
-    Vec1b* gy_row = gradY.ptr<Vec1b>(i);
-    Vec1b* src_row = src.ptr<Vec1b>(i);
-    for (int j = 1; j < src.cols - 1; j++)
+    cv::Vec<uchar, 1>* gx_row = gradientX.ptr<cv::Vec<uchar, 1> >(i);
+    cv::Vec<uchar, 1>* gy_row = gradientY.ptr<cv::Vec<uchar, 1> >(i);
+    cv::Vec<uchar, 1>* dest_row = dest->ptr<cv::Vec<uchar, 1> >(i);
+    for (int j = 1; j < C - 1; j++)
     {
       uchar GX = gx_row[j][0];
       uchar GY = gy_row[j][0];
-      int G = 0.5 * (int) std::round(std::hypot((double) GX, (double) GY));
-      src_row[j][0] = G > 255 ? 100 : ((uchar) G);
+      int G = (int) std::round(std::hypot((double) GX, (double) GY));
+      dest_row[j][0] = G > 255 ? 100 : ((uchar) G);
     }
   }
 
-  delete &gradX, &gradY;
+  return *dest;
 }
 
 /* Applies given threshold to the src image.
